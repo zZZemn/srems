fd2cbbdacbf2a0314f87931d54c12019df08a2b7
Emmanuel Ugaban <ugabanemmanuel@outlook.com>
2024-10-17 21:06:04 +0800

    backup info

--- pages/Backup.php
+++ pages/Backup.php
@@ -1,8 +1,7 @@
 <?php include("components/header.php") ?>
 
-<h4 class="text-primary">Back up</h4>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
-<!-- <a href="http://localhost/phpmyadmin/index.php?route=/database/export&db=SREMS" target="_blank" class="btn btn-sm btn-primary">Export Database</a> -->
+<h4 class="text-primary">Database Backup</h4>
+<p class="alert alert-secondary">Create a backup of your database to ensure data is preserved in case of emergencies or system failures.</p>
 <a href="../backend/controller/backup/BackupController.php" target="_blank" class="btn btn-sm btn-primary">Export Database</a>
 
 
