9b3c3440f18865b77483bd37db8509d3576fa4a2
Emmanuel Ugaban <ugabanemmanuel@outlook.com>
2024-11-14 21:33:09 +0800

    make password hash

--- backend/controller/POST.php
+++ backend/controller/POST.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -6,44 +6,37 @@ ini_set('display_errors', 1);
 include('../query.php');
 $query = new Query();
 
-if(isset($_POST['REQUEST_TYPE'])) {
+if (isset($_POST['REQUEST_TYPE'])) {
     $reqType = $_POST['REQUEST_TYPE'];
 
-    if($reqType == 'LOGIN') {
+    if ($reqType == 'LOGIN') {
 
-        if(isset($_POST['username'], $_POST['password'])) {
+        if (isset($_POST['username'], $_POST['password'])) {
 
             $username = $_POST['username'];
             $password = $_POST['password'];
-        
+
             $login = $query->login($username);
-        
-            if($login->num_rows > 0) {
+
+            if ($login->num_rows > 0) {
 
                 $loginResult = $login->fetch_assoc();
                 $loginPassword = $loginResult['PASSWORD'];
-                if ($loginPassword == $password) {
-        
+                if (password_verify($password, $loginPassword)) {
+
                     session_start();
                     $_SESSION['id'] = $loginResult['ID'];
                     echo 200;
-        
                 } else {
                     echo 400;
                 }
-                
             } else {
                 echo 400;
             }
-
         } else {
             echo 400;
         }
-
-    }  else {
+    } else {
         echo 400;
     }
-
 }
-
-?>
